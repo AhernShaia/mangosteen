5695701565350fcdb6ea90fc198fab9bfd9f0fac
AhernShaia <gemgt4gtii@gmail.com>
2023-03-13 22:21:13 +0800

    auto pack and build image script

---
+++ bin/pack_for_host.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+dir=mangosteen
+
+time=$(date +'%Y%m%d-%H%M%S')
+dist=tmp/mangosteen-$time.tar.gz
+current_dir=$(dirname $0)
+deploy_dir=/Users/ahern/Documents/5xRuby/rails7/mangosteen_deploy
+
+yes | rm tmp/mangosteen-*.tar.gz; 
+yes | rm $deploy_dir/mangosteen-*.tar.gz; 
+
+
+
+tar --exclude="tmp/cache/*" -czv -f $dist *
+mkdir -p $deploy_dir
+cp $current_dir/../config/host.Dockerfile $deploy_dir/Dockerfile
+cp $current_dir/setup_host.sh $deploy_dir/
+mv $dist $deploy_dir
+echo $time > $deploy_dir/version
+echo 'DONE!'
